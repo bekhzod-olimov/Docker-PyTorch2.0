9d6c48569c057ad627f2071ce3c92a92ad940227
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-01-19 12:24:49 +0900

    Update transforms.py

--- transforms.py
+++ transforms.py
@@ -2,6 +2,12 @@ import torchvision.transforms as tfs
 
 def get_tfs(ds_name):
     
+    """
+    
+    Gets dataset name and returns transforms.
+    
+    """
+    
     if ds_name == "cifar10" or ds_name == "cifar100":
         
         transform = tfs.Compose([tfs.ToTensor(), tfs.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
