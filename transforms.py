e478ac0015bfba2199434021eccc2e42edc4d44d
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-01-19 12:25:26 +0900

    Update transforms.py

--- transforms.py
+++ transforms.py
@@ -6,8 +6,12 @@ def get_tfs(ds_name):
     
     Gets dataset name and returns transforms.
     
+    Arguments:
+    ds_name - dataset name.
+    
     """
     
+    # CIFAR10 and CIFAR100
     if ds_name == "cifar10" or ds_name == "cifar100":
         
         transform = tfs.Compose([tfs.ToTensor(), tfs.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
