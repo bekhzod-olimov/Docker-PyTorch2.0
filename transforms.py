f03ae555e0148399858855f064b238d5a427dfc7
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-06-09 09:37:56 +0900

    transformations script

--- transforms.py
+++ transforms.py
@@ -7,24 +7,10 @@ def get_tfs(ds_name):
     
     This function gets dataset name and returns transforms.
     
-    Arguments:
+    Parameter:
     
-        ds_name - dataset name.
+        ds_name - dataset name, str.
     
     """
     
-    # CIFAR10 and CIFAR100
-    if ds_name == "cifar10" or ds_name == "cifar100":
-        
-        # Tensor and Standard Normalization
-        transform = tfs.Compose([tfs.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)), tfs.ToTensor()])
-        
-        return transform
-    
-    # MNIST 
-    elif ds_name == "mnist":
-        
-        # Tensor and Standard Normalization
-        transform = tfs.Compose([tfs.Normalize((0.5), (0.5)), tfs.ToTensor()])
-        
-        return transform
+    return tfs.Compose([tfs.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)), tfs.ToTensor()]) if "cifar" in ds_name else tfs.Compose([tfs.Normalize((0.5), (0.5)), tfs.ToTensor()])
